943de49865cbc0d502c7b85617e7c32608991626
anuragGUPTA2235 <161227082+anuragGUPTA2235@users.noreply.github.com>
2024-02-26 11:33:50 +0530

    Time: 1686 ms (26.41%) | Memory: 17.5 MB (75.59%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,12 +2,13 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         print(nums)
         print(target)
-        """
+        
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     print(i,nums[j])
                     return i,j
+    
         """
         list = {}
         for index,items in enumerate(nums):
@@ -18,5 +19,6 @@ class Solution:
             residual = target - nums[i]
             if residual in nums[i+1:]:
                 print(residual)
-                return i,list[residual]            
+                return i,list[residual]  
+        """                  
         
