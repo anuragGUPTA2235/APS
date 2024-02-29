1bff4b038bc8cff60bd219c73ce4a68e5fd1ee04
anuragGUPTA2235 <161227082+anuragGUPTA2235@users.noreply.github.com>
2024-02-29 19:27:32 +0530

    Time: 353 ms (39.38%) | Memory: 17.9 MB (45.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,14 +2,15 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         print(nums)
         print(target)
-        
+        """ brute force
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     print(i,nums[j])
                     return i,j
+        """            
     
-        """
+        
         list = {}
         for index,items in enumerate(nums):
             list[items] = index
@@ -20,5 +21,20 @@ class Solution:
             if residual in nums[i+1:]:
                 print(residual)
                 return i,list[residual]  
-        """                  
+        """      
+        dic = {}
+        for index,items in enumerate(nums):
+            dic[items] = index
+        nums.sort()
+        print(nums)
+        left  = 0 
+        right  = len(nums)-1
+        for i in range(len(nums)):
+            if nums[left] + nums[right] >  target:
+                right  = right - 1
+            elif nums[left] +  nums[right] < target:
+                left = left + 1
+            else:
+                return dic[nums[left]],dic[nums[right]]  
+        """                         
         
