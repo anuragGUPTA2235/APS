0b9ea8d30bf8889b6e62bed71cde0501ca82e315
Anurag Gupta <shekharanurag2235@gmail.com>
2024-03-31 14:04:58 +0530

    Time: 109 ms (23.64%) | Memory: 17.9 MB (8.43%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
@@ -1,10 +1,36 @@
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        # brute force
-        count = 0
-        for items in grid:
-            for jtems in items:
-                if jtems < 0:
-                 count += 1
-        return count        
+ def countNegatives(self, grid: List[List[int]]) -> int:
+      
+
+             # efficient way
+             def bs_nested(nums):
+                        countneg = 0
+            
+                        for i in range(len(grid)):
+                            grid[i] = grid[i][::-1] 
+                        print(grid)     
+                        for i in range(len(grid)):
+                             left = 0
+                             right = len(grid[0])-1
+                             while left <= right:
+                               mid = (left + right) // 2
+                            
+                               if grid[i][mid] < 0:
+                                   left = mid + 1
+                               else:
+                                   right = mid - 1
+                            
+                               
+                             countneg += (left) 
+                             
+    
+                        return countneg     
+
+
+             num = bs_nested(grid)
+             return num        
+
+
+        
+
         
